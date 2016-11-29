41d118312821d039255d1bd1e0f067c3664a121b
Nguyễn Thuận <thuannv.hust@outlook.com>
2016-11-29 17:04:53 +0700

    add template and change menu in ASP.NET MVC5

---
+++ WebApplication1/WebApplication1/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class DataContext : DbContext
+    {
+        public System.Data.Entity.DbSet<WebApplication1.Models.Category> Categories { get; set; }
+
+        public System.Data.Entity.DbSet<WebApplication1.Models.Product> Products { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public String ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
